82fe641756e97484fb5694b346823a7e31fd5f52
Soenke Beyer <soenke.t.beyer@student.fh-kiel.de>
2020-05-17 20:20:30 +0200

    addet comments

--- Arduino_Guitar_Amp/echo.h
+++ Arduino_Guitar_Amp/echo.h
@@ -1,3 +1,11 @@
+/*
+ * This File contains the reverb of the Amp. The Arduino has very limitet
+ * amount of memory and so the length of the reverb is very limited. The
+ * buffer is set to the max limit. Unlike distorsions you can not choose
+ * the level of the reverb yourself because the range is so limited and is
+ * already at the max limit.
+ */
+
 int reverbBuffer[800];
 int reverbBufferCounter = 0;
 
